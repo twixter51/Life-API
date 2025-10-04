aa4511307d9aa60d001593feac6345879125b8ba
twixt <twixterdark@gmail.com>
2025-10-03 20:55:30 -0700

    First Commit

---
+++ README.md
@@ -0,0 +1,69 @@
+# Life's API
+
+# This API leverages posts from any reddit community, preferably a inspirational community that posts quotes, images with quotes etc. 
+
+
+# Current Version 0.1
+
+
+- Implemented Tesseract OCR so now images and their quotes are now transferred into a semi-readable string.
+
+- Future will include AI integration where I clean up the quotes and package them nicely with the image.
+
+- Expect constant updates as I am constantly going to improve this API as much as possible, I don't want praise for making this I just 
+
+
+
+
+# notes on FAST API (I am learning this with you so take my notes as is and confirm with your own research)
+
+- So FASTAPI leverages Type Hints to add things such as metadata(data about data, basically for a picture it would be it's Date taken etc.) or anything else.
+
+- Type Hints used in python are declared as follows:
+
+    # def someFunction(somevariable : str):
+            # somevariable. (now some variable has properties that it wouldn't have otherwise, aka for strings in this case)
+
+- Some things to remember is that Type Hints can be declared in many ways depending on the version of python you are running, so make sure to use the best format!
+
+    # Some Notes taken from FAST API DOC
+        from typing import Optional
+
+        def say_hi(name: Optional[str] = None):
+            if name is not None:
+                print(f"Hey {name}!")
+            else:
+                print("Hello World")
+        Using Optional[str] instead of just str will let the editor help you detect errors where you could be assuming that a value is always a str, when it could actually be None too.
+
+        Optional[Something] is actually a shortcut for Union[Something, None], they are equivalent.
+
+        This also means that in Python 3.10, you can use Something | None:
+
+- FastAPI also uses pythons new Pydantic Library to declare properties for data types that are instances of an class EX: 
+        from datetime import datetime
+
+        from pydantic import BaseModel
+
+
+        class User(BaseModel):
+            id: int
+            name: str = "John Doe"
+            signup_ts: datetime | None = None
+            friends: list[int] = []
+
+
+        external_data = {
+            "id": "123",
+            "signup_ts": "2017-06-01 12:22",
+            "friends": [1, "2", b"3"],
+        }
+        user = User(**external_data)
+        print(user)
+        # > User id=123 name='John Doe' signup_ts=datetime.datetime(2017, 6, 1, 12, 22) friends=[1, 2, 3]
+        print(user.id)
+        # > 123
+
+
+-- end of notes ( more to be added soon as we progress into this new API (suggestions are welcome so do contribute if you believe you'd want to change something))
+
---
+++ main.py
@@ -0,0 +1,159 @@
+from fastapi import FastAPI, Query
+from pydantic import BaseModel, HttpUrl
+from pydantic import Field
+from typing import Literal
+from typing import Optional
+from fastapi import Depends, Header, HTTPException, status
+from PIL import Image
+from io import BytesIO
+import asyncio
+import httpx
+import random
+
+import pytesseract
+
+pytesseract.pytesseract.tesseract_cmd = r"C:\Program Files\Tesseract-OCR\tesseract.exe"
+
+#auth
+
+from pydantic_settings import BaseSettings
+
+class Settings(BaseSettings):
+    API_KEY: str = "dev-secret"  # fallback for local
+    class Config:
+        env_file = ".env"
+
+settings = Settings()
+
+
+async def require_api_key(x_api_key: str | None = Header(default=None)):
+    if x_api_key != settings.API_KEY:
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Invalid or missing API key")
+    return True
+
+# end of auth
+
+
+# (temporary documnentation) This API allows you to grab any inspiritional quote directly from r/inspiration on reddit. Although most are images (AI will be implemented later to counteract this)
+
+app = FastAPI()
+
+
+class PostPreview(BaseModel):
+    id: str
+    title: str
+    score: int = Field(ge=0)
+    permalink: HttpUrl
+    author: Optional[str] = None
+    date: float
+    image: Optional[str] = None
+    image_quote: Optional[str] = None
+
+class SubredditResponse(BaseModel):
+    subreddit: str
+    limit: int
+    posts: list[PostPreview]
+    sort: Literal["hot", "new", "top"]
+    count: int
+
+async def image_to_bytes(img_url: str):
+
+    async with httpx.AsyncClient() as client:
+        response = await client.get(img_url)      # <-- downloads raw bytes
+        response.raise_for_status()
+    img = Image.open(BytesIO(response.content))  # <-- convert bytes to Pillow image
+    return img
+
+
+async def fetch_posts(subreddit: str, limit: int, sort:Literal["hot", "new", "top"]):
+    url = f"https://www.reddit.com/r/{subreddit}/{sort}.json?limit={50}"
+    headers = {"User-Agent": "fastapi-learning-app/0.1"}
+    sets = ["png", "jpg", "jpeg", "webp"]
+    image = ""
+    NEW_POSTS = []
+
+
+
+    # add error handling later
+        
+
+    # Include more types, for example if it's a gif then....its skipped
+    
+    while len(NEW_POSTS) < 90:
+       
+        async with httpx.AsyncClient() as client:
+            response = await client.get(url, headers=headers)
+            response.raise_for_status()
+            data = response.json()
+        JSONDATA = data.get("data", {}).get("children", [])
+
+        after = data.get("data", {}).get("after")
+
+        for d in JSONDATA:
+
+            image = d.get("data", {}).get("url", "")
+       
+            if image == ".jpg" or image == ".png":
+                image = image[-3:]
+            else:
+                image = image[-4:]
+
+            if image in sets:
+                NEW_POSTS.append(PostPreview.model_validate({
+                    "title": d.get("data", {}).get("title", ""),
+                    "score": d.get("data", {}).get("score", 0),
+                    "id": d.get("data", {}).get("id", ""),
+                    "permalink": "https://www.reddit.com" + d.get("data", {}).get("permalink", "/"),
+                    "author": d.get("data", {}).get("author", ""),
+                    "date": d.get("data", {}).get("created_utc", 0.0),
+                    "image": d.get("data", {}).get("url", ""),
+                    "image_quote": "error"
+                }).model_dump())
+                   
+        
+
+        if not after:
+            break
+        url = f"https://www.reddit.com/r/{subreddit}/{sort}.json?limit={50}&after={after}"
+        await asyncio.sleep(3) 
+
+    
+
+
+   
+       
+        
+    random.shuffle(NEW_POSTS)
+    NEW_POSTS = NEW_POSTS[:limit]
+
+
+    for entry in NEW_POSTS:
+        entry["image_quote"] = pytesseract.image_to_string(await image_to_bytes(entry["image"]))
+            
+ 
+
+
+    return NEW_POSTS
+    
+
+
+
+@app.get("/reddit/{subreddit}", 
+    response_model=SubredditResponse,
+    summary="List posts from a subreddit, mainly r/inspiration",
+    description="N/A",         
+)
+async def get_inspiration_from_reddit(subreddit: str, limit: int = Query(5, ge=1, le=25), sort: Literal["hot", "new", "top"] = "hot", _auth: bool = Depends(require_api_key)):
+    subreddit = subreddit.lower()
+
+
+    posts = await fetch_posts(subreddit, limit, sort)
+    
+    return {
+        "subreddit": subreddit,
+        "limit": limit,
+        "posts": posts,
+        "sort": sort,
+        "count": len(posts),
+    }
+
